6f8a9a570c94ae47f6f7fa95134e4a113f44f9c4
Abdulhabir <102049496+AbdulhabirKarahanli@users.noreply.github.com>
2023-07-05 16:43:12 +0300

    Add files via upload
    A very basic introduction to Python.

---
+++ Introduction.py
@@ -0,0 +1,27 @@
+# Making a Triangle!
+
+print("   /|") #first this line is executed.
+print("  / |")
+print(" /  |")
+print("/___|")
+
+
+# Variables
+
+character_name = "Tom"
+character_age = "50"
+
+print("There once was a man named " + character_name + ",")
+print("he was " + character_age + " years old")
+print("He really like the name " + character_name + ",")
+print("but didn't like being " + character_age + ".")
+
+# Three basic data types:
+    # 1. Strings, for playing texts.
+    # 2. Numbers (decimals and all types of numbers included)
+    # 3. Boolean (True or False)
+
+
+
+
+
